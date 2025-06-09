be0f4562dbd885f10a7921797a416af485e4d9c4
thinhnc <congthinhq@gmail.com>
2025-06-09 12:00:18 +0700

    :sparkles: Add ping

---
+++ app/api/ping/route.ts
@@ -0,0 +1,9 @@
+export const runtime = 'edge'
+export const dynamic = 'force-static'
+
+export async function GET() {
+  return new Response('ok', {
+    status: 200,
+    headers: { 'content-type': 'text/plain' }
+  })
+}
