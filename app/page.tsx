d8c5433cfebe70a98bbf5ed7d991b525174e9bd8
thinhnc <congthinhq@gmail.com>
2025-06-10 11:51:24 +0700

    :lipstick: Align spacing

--- app/page.tsx
+++ app/page.tsx
@@ -157,7 +157,7 @@ export default function HomePage() {
         </div>
       </section>
       {/* About Us Section */}
-      <section className='py-8 md:py-16 bg-white'>
+      <section className='py-6 md:py-16 bg-white'>
         <div className='max-w-7xl mx-auto px-4 text-center'>
           <h2 className='text-4xl font-bold text-primary mb-2 md:mb-4 tracking-wider'>
             GIỚI THIỆU
@@ -170,12 +170,6 @@ export default function HomePage() {
                 mỗi vách ngăn khói hay thiết bị PCCC đều là minh chứng cho cam
                 kết vững chắc vì sự an toàn cộng đồng.
               </p>
-              <Link
-                href='/gioi-thieu'
-                className='inline-block mt-4 px-4 py-2 bg-primary text-white rounded-lg shadow hover:scale-105 transition-transform duration-300 font-semibold'
-              >
-                Xem thêm
-              </Link>
             </div>
             <div className='max-w-[1000px] mx-auto text-center bg-gray-100 rounded-lg shadow-md p-2 mt-4 md:mt-8 hover:shadow-lg transition-shadow duration-300'>
               <Image
@@ -188,11 +182,17 @@ export default function HomePage() {
                 Giới thiệu Công ty Cổ phần Cơ khí Xây dựng Trần Gia Phát
               </p>
             </div>
+            <Link
+              href='/gioi-thieu'
+              className='inline-block mt-6 px-4 py-2 bg-primary text-white rounded-lg shadow hover:scale-105 transition-transform duration-300 font-semibold'
+            >
+              Xem thêm
+            </Link>
           </div>
         </div>
       </section>
       {/* Products Section */}
-      <section className='py-8 md:py-16 bg-white'>
+      <section className='py-6 md:py-16 bg-gray-50'>
         <div className='max-w-7xl mx-auto px-4'>
           <h2 className='text-3xl font-bold text-center mb-12 text-primary'>
             SẢN PHẨM NỔI BẬT
--- components/productCard.tsx
+++ components/productCard.tsx
@@ -11,7 +11,6 @@ export interface ProductCardProps {
   description: string
 }
 
-// Định nghĩa props để nhận dữ liệu sản phẩm
 const ProductCard = (product: ProductCardProps) => {
   const { name, href, image, alt, description } = product
   return (
@@ -20,28 +19,20 @@ const ProductCard = (product: ProductCardProps) => {
       className='group block h-full' // block và h-full là quan trọng để Link hoạt động như một block và đảm bảo chiều cao card đồng đều
     >
       <div className='bg-gray-100 rounded-lg overflow-hidden flex flex-col h-full'>
-        {' '}
-        {/* flex-col và h-full ở đây */}
-        <div className='relative w-full h-48 overflow-hidden'>
-          {' '}
-          {/* Container cho Image */}
+        <div className='relative w-full h-48 overflow-hidden p-2'>
           <Image
             src={image}
             alt={alt}
             width={300} // Cung cấp kích thước gốc của ảnh để Next.js tối ưu
             height={300} //
-            className='w-full object-cover group-hover:scale-105 transition-transform duration-300'
+            className='w-full object-cover group-hover:scale-105 transition-transform duration-300 rounded'
           />
         </div>
         <div className='p-6 flex flex-col flex-grow'>
-          {' '}
-          {/* flex-col và flex-grow ở đây */}
           <h3 className='text-xl font-semibold mb-2 group-hover:text-blue-600 uppercase'>
             {name}
           </h3>
           <p className='text-gray-600 text-sm flex-grow line-clamp-3'>
-            {' '}
-            {/* flex-grow và line-clamp-3 */}
             {description}
           </p>
         </div>
